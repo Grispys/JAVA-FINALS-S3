3a15758e533ef6be99d5fe6e41b11d7c8a86619b
Joshua Youden <Joshua.Youden@keyin.com>
2024-12-02 13:26:59 -0330

    Finished Byers.java

--- Buyers.java
+++ Buyers.java
@@ -4,6 +4,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+
 public class Buyers extends Users{
 
 	public Buyers(String username, String email, String password, String role){
